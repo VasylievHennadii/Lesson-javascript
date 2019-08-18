308d33082a01dad72799a15604bed1c4be419d85
BrooonS <brooons@yandex.ru>
2019-08-19 00:31:17 +0300

    test homework

--- hw2/task-5.js
+++ hw2/task-5.js
@@ -1,22 +1,27 @@
 // Задание 5. Функция принимает число n. Выдать первые n чисел ряда фибоначи.
 
 console.log('Задание 5. Функция принимает число n. Выдать первые n чисел ряда фибоначи');
+
 function myFibonacci(answer) {
-    if (parseInt(answer) === +answer && +answer > 0) {
-        var first = 0;
-        var second = 1;
-        var sum = 0;
-        console.log(first);
-        console.log(second);
-        for (var i = 3; i <= answer; i++) {    
-            sum = first + second;      
-            first = second;
-            second = sum;   
-            console.log(sum);
-        }
-    } else {
-        alert('введите целое положительное число');
-    }    
-};
-var answer = prompt('enter');
+  if (parseInt(answer) === +answer && +answer > 0) {
+    var first = 0;
+    var second = 1;
+    var sum = 0;
+    // Лучше собрать результат в переменную и потом один раз вывести
+    var string = first + ', ' + second;
+
+    for (var i = 3; i <= answer; i++) {
+      sum = first + second;
+      first = second;
+      second = sum;
+      string += ', ' + sum;
+    }
+
+    alert(string);
+  } else {
+    alert('введите целое положительное число');
+  }
+}
+
+var answer = prompt('Введите число');
 var result = myFibonacci(answer);
